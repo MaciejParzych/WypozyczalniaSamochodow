3f37b59e07017eb6e3da57ed73f4fd2ff1e5aa1b
giciarz412 <tomaszfefler1990@gmail.com>
2019-05-27 20:32:18 +0200

    ostateczna wersja polska 5 BARDZO WAZNY COMMIT2

--- src/main/java/WypozyczalniaSamochodow/gui/GlowneOkno.java
+++ src/main/java/WypozyczalniaSamochodow/gui/GlowneOkno.java
@@ -202,19 +202,23 @@ public class GlowneOkno extends JFrame {
         return this.przyciskWDol;
     }
 
-     void uaktualnijLicznik(String str1,  String carName, String s, int n ){
+    int uaktualnijLicznik(String str1, String carName, String s, int n) {
         if (str1.equals("t")) {
 
             todoListModel.dodaj(carName + "     " + getNewTaskField().getText() + "         " + s + "         " + (n * 1000) + " zł za okres tygodnia");
+            return 0;
         }
         if (str1.equals("m")) {
-            licznik+=3;
-            todoListModel.dodaj(carName + "     " + getNewTaskField().getText() + "         " + s + "         " + (4*n * 1000) + " zł za okres miesiąca");
+            licznik += 3;
+            todoListModel.dodaj(carName + "     " + getNewTaskField().getText() + "         " + s + "         " + (4 * n * 1000) + " zł za okres miesiąca");
+            return 1;
         }
         if (str1.equals("r")) {
-            licznik=licznik+
-            todoListModel.dodaj(carName + "     " + getNewTaskField().getText() + "         " + s + "         " + (19*n * 1000) + " zł za okres roku");
+            licznik = licznik + (n * 29);
+            todoListModel.dodaj(carName + "     " + getNewTaskField().getText() + "         " + s + "         " + (19 * n * 1000) + " zł za okres roku");
+            return 2;
         }
+        return 3;
     }
 
     public void mouseClickedMethod(JButton button, int n, String carName) {
@@ -228,124 +232,134 @@ public class GlowneOkno extends JFrame {
                 String s = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
 
 
-                (new GlowneOkno()).uaktualnijLicznik(str1, carName, s,n);
-
-
                 licznik += n;
+                int count = (new GlowneOkno()).uaktualnijLicznik(str1, carName, s, n);
+                switch (count) {
+                    case 0:
+                        todoListModel.dodaj(carName + "     " + getNewTaskField().getText() + "         " + s + "         " + (n * 1000) + " zł za okres tygodnia");
+                        break;
+                    case 1:
+                        licznik += 3;
+                        todoListModel.dodaj(carName + "     " + getNewTaskField().getText() + "         " + s + "         " + (4 * n * 1000) + " zł za okres miesiąca");
+                        break;
+                    case 2:
+                        licznik = licznik + (n * 29);
+                        todoListModel.dodaj(carName + "     " + getNewTaskField().getText() + "         " + s + "         " + (30 * n * 1000) + " zł za okres roku");
+                        break;
+
+                }
+
 
                 if (getNewTaskField().getText().length() > 0) {
 
 
+                    if (getNewTaskField().getText().length() > 100) {
+                        try {
+                            throw new ZbytDuzoZnakow();
 
-
-                        if (getNewTaskField().getText().length() > 100) {
-                            try {
-                                throw new ZbytDuzoZnakow();
-
-                            } catch (ZbytDuzoZnakow tooManyCharacters) {
-                                JOptionPane.showMessageDialog(dodajPrzycisk,
-                                        "Wprowadź maksymalnie 100 znaków.",
-                                        "Błąd", JOptionPane.ERROR_MESSAGE);
-                            }
-                        } else {
-                            getTaskList().setSelectedIndex(getTaskList().getModel().getSize() - 1);
+                        } catch (ZbytDuzoZnakow tooManyCharacters) {
+                            JOptionPane.showMessageDialog(dodajPrzycisk,
+                                    "Wprowadź maksymalnie 100 znaków.",
+                                    "Błąd", JOptionPane.ERROR_MESSAGE);
                         }
+                    } else {
+                        getTaskList().setSelectedIndex(getTaskList().getModel().getSize() - 1);
                     }
+                }
+            }
+        });
+    }
+
+
+    private JButton getAddTaskButton() {
+        if (this.dodajPrzycisk == null) {
+            this.dodajPrzycisk = new JButton();
+            this.dodajPrzycisk.setIcon(createIcon("bmw.png"));
+
+
+            mouseClickedMethod(dodajPrzycisk, 1, "BMW 5 Series");
+
+        }
+
+        return this.dodajPrzycisk;
+    }
+
+
+    private JButton getAddTaskButton2() {
+        if (this.dodajPrzycisk2 == null) {
+            this.dodajPrzycisk2 = new JButton();
+            this.dodajPrzycisk2.setIcon(createIcon("jeep.png"));
+
+
+            mouseClickedMethod(dodajPrzycisk2, 1, "Jeep Grand Cherokee");
+
+        }
+
+        return this.dodajPrzycisk2;
+    }
+
+
+    private JButton getAddTaskButton3() {
+        if (this.dodajPrzycisk3 == null) {
+            this.dodajPrzycisk3 = new JButton();
+            this.dodajPrzycisk3.setIcon(createIcon("toyota.png"));
+
+
+            mouseClickedMethod(dodajPrzycisk3, 1, "Toyota Vios 1.3 xe");
+
+        }
+
+        return this.dodajPrzycisk3;
+    }
+
+
+    private JButton getAddTaskButton4() {
+        if (this.dodajPrzycisk4 == null) {
+            this.dodajPrzycisk4 = new JButton();
+            this.dodajPrzycisk4.setIcon(createIcon("ferrari.png"));
+
+
+            mouseClickedMethod(dodajPrzycisk4, 3, "Ferrari f8 Tributo");
+
+        }
+
+        return this.dodajPrzycisk4;
+    }
+
+
+    private JLabel getStatusBar() {
+        if (this.pasekStatusu == null) {
+            this.pasekStatusu = new JLabel("Liczba wypożyczonych samochodów: 0. Opłaty łącznie: 0zł");
+
+            this.todoListModel.addListDataListener(new ListDataListener() {
+                @Override
+                public void contentsChanged(ListDataEvent e) {
+                    updateLabel(e);
+                }
+
+                private void updateLabel(ListDataEvent e) {
+                    getStatusBar().setText("Liczba wypożyczonych samochodów: " +
+                            ((WypozyczalniaModel) e.getSource()).getSize() + ". Opłaty łącznie: " + licznik * 1000 + "zł");
+                }
+
+                @Override
+                public void intervalRemoved(ListDataEvent e) {
+                }
+
+                @Override
+                public void intervalAdded(ListDataEvent e) {
                 }
             });
         }
 
-
-        private JButton getAddTaskButton () {
-            if (this.dodajPrzycisk == null) {
-                this.dodajPrzycisk = new JButton();
-                this.dodajPrzycisk.setIcon(createIcon("bmw.png"));
-
-
-                mouseClickedMethod(dodajPrzycisk, 1, "BMW 5 Series");
-
-            }
-
-            return this.dodajPrzycisk;
-        }
-
-
-        private JButton getAddTaskButton2 () {
-            if (this.dodajPrzycisk2 == null) {
-                this.dodajPrzycisk2 = new JButton();
-                this.dodajPrzycisk2.setIcon(createIcon("jeep.png"));
-
-
-                mouseClickedMethod(dodajPrzycisk2, 1, "Jeep Grand Cherokee");
-
-            }
-
-            return this.dodajPrzycisk2;
-        }
-
-
-        private JButton getAddTaskButton3 () {
-            if (this.dodajPrzycisk3 == null) {
-                this.dodajPrzycisk3 = new JButton();
-                this.dodajPrzycisk3.setIcon(createIcon("toyota.png"));
-
-
-                mouseClickedMethod(dodajPrzycisk3, 1, "Toyota Vios 1.3 xe");
-
-            }
-
-            return this.dodajPrzycisk3;
-        }
-
-
-        private JButton getAddTaskButton4 () {
-            if (this.dodajPrzycisk4 == null) {
-                this.dodajPrzycisk4 = new JButton();
-                this.dodajPrzycisk4.setIcon(createIcon("ferrari.png"));
-
-
-                mouseClickedMethod(dodajPrzycisk4, 3, "Ferrari f8 Tributo");
-
-            }
-
-            return this.dodajPrzycisk4;
-        }
-
-
-
-        private JLabel getStatusBar () {
-            if (this.pasekStatusu == null) {
-                this.pasekStatusu = new JLabel("Liczba wypożyczonych samochodów: 0. Opłaty łącznie: 0zł");
-
-                this.todoListModel.addListDataListener(new ListDataListener() {
-                    @Override
-                    public void contentsChanged(ListDataEvent e) {
-                        updateLabel(e);
-                    }
-
-                    private void updateLabel(ListDataEvent e) {
-                        getStatusBar().setText("Liczba wypożyczonych samochodów: " +
-                                ((WypozyczalniaModel) e.getSource()).getSize() + ". Opłaty łącznie: " +  licznik * 1000 + "zł");
-                    }
-
-                    @Override
-                    public void intervalRemoved(ListDataEvent e) {
-                    }
-
-                    @Override
-                    public void intervalAdded(ListDataEvent e) {
-                    }
-                });
-            }
-
-            return this.pasekStatusu;
-        }
-
-        private Icon createIcon (String iconfilename){
-            return new ImageIcon(
-                    getClass().
-                            getResource("/" + iconfilename));
-        }
-
-
+        return this.pasekStatusu;
     }
+
+    private Icon createIcon(String iconfilename) {
+        return new ImageIcon(
+                getClass().
+                        getResource("/" + iconfilename));
+    }
+
+
+}
